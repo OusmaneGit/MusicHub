e79e58285e17b75c9c16b27ee6d1d92ab7368817
OUSMANE <ba.ousmane@hotmail.com>
2017-08-13 19:27:35 +0200

    Create add a concert form

---
+++ MusicHub/Controllers/ConcertsController.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Linq;
+
+using System.Web.Mvc;
+using MusicHub.Models;
+using MusicHub.ViewModels;
+
+namespace MusicHub.Controllers
+{
+    public class ConcertsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ConcertsController()
+        {
+            _context= new ApplicationDbContext();
+        }
+        
+        public ActionResult Create()
+        {
+            var viewModel= new ConcertFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ MusicHub/ViewModels/ConcertFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+
+using MusicHub.Models;
+
+namespace MusicHub.ViewModels
+{
+    public class ConcertFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
